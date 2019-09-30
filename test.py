94a1104c32b5d3f5dafa237da8621e04cc44bec6
kamilbozek <kamil.bozek@gmail.com>
2019-09-30 11:03:03 +0200

    test google mail service

---
+++ test.py
@@ -0,0 +1,264 @@
+from __future__ import print_function
+import pickle
+import os.path
+import base64
+import mimetypes
+import os
+import time
+from apiclient import errors
+from email.mime.audio import MIMEAudio
+from email.mime.base import MIMEBase
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/gmail.modify']
+
+def get_message(service, user_id, msg_id):
+    """Get a Message with given ID.
+
+    Args:
+      service: Authorized Gmail API service instance.
+      user_id: User's email address. The special value "me"
+      can be used to indicate the authenticated user.
+      msg_id: The ID of the Message required.
+
+    Returns:
+      A Message.
+    """
+    try:
+        message = service.users().messages().get(userId=user_id, id=msg_id).execute()
+
+        print('Message snippet: %s' % message['snippet'])
+
+        return message
+    except errors.HttpError as error:
+        print('An error occurred: %s' % error)
+
+
+def list_messages_matching_query(service, user_id, query=''):
+    """List all Messages of the user's mailbox matching the query.
+
+    Args:
+      service: Authorized Gmail API service instance.
+      user_id: User's email address. The special value "me"
+      can be used to indicate the authenticated user.
+      query: String used to filter messages returned.
+      Eg.- 'from:user@some_domain.com' for Messages from a particular sender.
+
+    Returns:
+      List of Messages that match the criteria of the query. Note that the
+      returned list contains Message IDs, you must use get with the
+      appropriate ID to get the details of a Message.
+    """
+    try:
+        response = service.users().messages().list(userId=user_id,
+                                                   q=query).execute()
+        messages = []
+        if 'messages' in response:
+            messages.extend(response['messages'])
+
+        while 'nextPageToken' in response:
+            page_token = response['nextPageToken']
+            response = service.users().messages().list(userId=user_id, q=query,
+                                                       pageToken=page_token).execute()
+            messages.extend(response['messages'])
+
+        return messages
+    except errors.HttpError as error:
+        print("An error occurred: %s" % error)
+
+
+def send_message(service, user_id, message):
+    """Send an email message.
+
+    Args:
+      service: Authorized Gmail API service instance.
+      user_id: User's email address. The special value "me"
+      can be used to indicate the authenticated user.
+      message: Message to be sent.
+
+    Returns:
+      Sent Message.
+    """
+    try:
+        message = (service.users().messages().send(userId=user_id, body=message)
+                   .execute())
+        print('Message Id: %s' % message['id'])
+        return message
+    except errors.HttpError as error:
+        print('An error occurred: %s' % error)
+
+
+def create_message(sender, to, subject, message_text):
+    """Create a message for an email.
+
+    Args:
+      sender: Email address of the sender.
+      to: Email address of the receiver.
+      subject: The subject of the email message.
+      message_text: The text of the email message.
+
+    Returns:
+      An object containing a base64url encoded email object.
+    """
+    message = MIMEText(message_text)
+    message['to'] = to
+    message['from'] = sender
+    message['subject'] = subject
+    raw = base64.urlsafe_b64encode(message.as_bytes())
+    raw = raw.decode()
+    body = {'raw': raw}
+    return body
+
+
+def create_message_with_attachment(sender, to, subject, message_text, file_dir,
+                                   filename):
+    """Create a message for an email.
+
+    Args:
+      sender: Email address of the sender.
+      to: Email address of the receiver.
+      subject: The subject of the email message.
+      message_text: The text of the email message.
+      file_dir: The directory containing the file to be attached.
+      filename: The name of the file to be attached.
+
+    Returns:
+      An object containing a base64url encoded email object.
+    """
+    message = MIMEMultipart()
+    message['to'] = to
+    message['from'] = sender
+    message['subject'] = subject
+
+    msg = MIMEText(message_text)
+    message.attach(msg)
+
+    path = os.path.join(file_dir, filename)
+    content_type, encoding = mimetypes.guess_type(path)
+
+    if content_type is None or encoding is not None:
+        content_type = 'application/octet-stream'
+    main_type, sub_type = content_type.split('/', 1)
+    if main_type == 'text':
+        fp = open(path, 'rb')
+        msg = MIMEText(fp.read(), _subtype=sub_type)
+        fp.close()
+    elif main_type == 'image':
+        fp = open(path, 'rb')
+        msg = MIMEImage(fp.read(), _subtype=sub_type)
+        fp.close()
+    elif main_type == 'audio':
+        fp = open(path, 'rb')
+        msg = MIMEAudio(fp.read(), _subtype=sub_type)
+        fp.close()
+    else:
+        fp = open(path, 'rb')
+        msg = MIMEBase(main_type, sub_type)
+        msg.set_payload(fp.read())
+        fp.close()
+
+    msg.add_header('Content-Disposition', 'attachment', filename=filename)
+    message.attach(msg)
+
+    return {'raw': base64.urlsafe_b64encode(message.as_bytes()).decode()}
+
+
+def modify_message(service, user_id, msg_id, msg_labels):
+    """Modify the Labels on the given Message.
+
+    Args:
+      service: Authorized Gmail API service instance.
+      user_id: User's email address. The special value "me"
+      can be used to indicate the authenticated user.
+      msg_id: The id of the message required.
+      msg_labels: The change in labels.
+
+    Returns:
+      Modified message, containing updated labelIds, id and threadId.
+    """
+    try:
+        message = service.users().messages().modify(userId=user_id, id=msg_id,
+                                                    body=msg_labels).execute()
+
+        label_ids = message['labelIds']
+
+        print
+        var = 'Message ID: %s - With Label IDs %s' % (msg_id, label_ids)
+        return message
+    except errors.HttpError as error:
+        print('An error occurred: %s' % error)
+
+
+def create_read_labels():
+    """Create object to update labels.
+
+    Returns:
+      A label update object.
+    """
+    return {'removeLabelIds': ['UNREAD'], 'addLabelIds': []}
+
+
+def process_unread_messages(service):
+    messages = list_messages_matching_query(service, "me", "label:unread")
+    for message in messages:
+        msg = get_message(service, "me", message['id'])
+        print("MESSAGE: %s" % msg['payload']['headers'])
+
+        subject = next(header['value'] for header in msg['payload']['headers'] if header['name'].lower() == "subject")
+        print(subject)
+
+        if subject == "foto":
+            fromAddress = next(
+                header['value'] for header in msg['payload']['headers'] if header['name'].lower() == "from")
+            # fromAddress = "kamil.bozek@gmail.com"
+            fromAddressParsed = fromAddress.split('<')[1].split('>')[0]
+            print(fromAddressParsed)
+            response = create_message("Emma Eternal Magic", fromAddressParsed, 'Wiadomosc od Emmy', 'Wiadomosc')
+            response2 = create_message_with_attachment("Emma Eternal Magic", fromAddressParsed, "Ja", "",
+                                                       "/home/kamil/tmp",
+                                                       "emma.jpg")
+
+            send_message(service, "me", response2)
+
+            modify_message(service, 'me', message['id'], create_read_labels())
+
+
+def main():
+    """Shows basic usage of the Gmail API.
+    Lists the user's Gmail labels.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('gmail', 'v1', credentials=creds)
+
+    while True:
+        time.sleep(20)
+        process_unread_messages(service)
+
+
+if __name__ == '__main__':
+    main()
