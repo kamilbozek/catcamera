79283ae21a279ce567e8fe681e5488ed5a7ca725
kamilbozek <kamil.bozek@gmail.com>
2019-09-30 12:16:28 +0100

    secret env variable

--- catcamera/catcamera.py
+++ catcamera/catcamera.py
@@ -6,48 +6,49 @@ import json
 import os
 
 
-def process_unread_messages(mail_service, camera):
+def process_unread_messages(mail_service, camera, secret):
     messages = mail_service.list_messages_matching_query("me", "label:unread")
     for message in messages:
         msg = mail_service.get_message("me", message['id'])
-        print("MESSAGE: %s" % msg['payload']['headers'])
+        #print("MESSAGE: %s" % msg['payload']['headers'])
 
         subject = next(
             header['value'] for header in msg['payload']['headers'] if header['name'].lower() == "subject")
-        print(subject)
+        #print(subject)
 
-        if subject == "foto":
+        if subject == secret:
             fromAddress = next(
                 header['value'] for header in msg['payload']['headers'] if header['name'].lower() == "from")
-            # fromAddress = "kamil.bozek@gmail.com"
+            print
+            print("New photo request from: %s" % fromAddress)
             fromAddressParsed = fromAddress.split('<')[1].split('>')[0]
-            print(fromAddressParsed)
-            response = mail_service.create_message("Emma Eternal Magic", fromAddressParsed, 'Wiadomosc od Emmy',
-                                           'Wiadomosc')
 
-            photos_dir = os.environ['PICAMERA_DIR']
+            photos_dir = os.environ['CATCAMERA_PHOTOS_DIR']
             timestamp = datetime.utcnow().strftime('%Y%m%d%H%M%S')
             photo_name = "catcamera_%s.jpg" % timestamp
             photo_path = "%s/%s" % (photos_dir, photo_name)
             print("Taking new photo at: '%s'" % photo_path)
             camera.take_photo(photo_path)
 
-            response2 = mail_service.create_message_with_attachment("Emma Eternal Magic", fromAddressParsed, "Ja", "",
+            response = mail_service.create_message_with_attachment("Emma Eternal Magic", fromAddressParsed, "Ja", "",
                                                             photos_dir,
                                                             photo_name)
 
-            mail_service.send_message("me", response2)
+            print("Sending new photo to: %s" % fromAddressParsed)
+            mail_service.send_message("me", response)
 
             mail_service.modify_message('me', message['id'], mail_service.create_read_labels())
+            print
 
 
 def main():
-    credentials_dir = os.environ['CREDS_DIR']
+    credentials_dir = os.environ['CATCAMERA_CREDS_DIR']
+    secret = os.environ['CATCAMERA_SECRET']
     gmail_service = GmailService(credentials_dir)
     camera = Camera()
-    sleep_time = 10
+    sleep_time = 20
     while True:
-        process_unread_messages(gmail_service, camera)
+        process_unread_messages(gmail_service, camera, secret)
         print("Sleeping %d seconds..." % sleep_time)
         time.sleep(sleep_time)
 
